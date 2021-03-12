4fa8b70b3af09fb22aa11c699911a79c77e1afa9
Kazeem Asiwaju-Bello <kasiwaju@axamansard.com>
2021-03-12 15:26:31 +0100

    SignIn page linked to Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 import Landingpage from "./pages/Landing";
 import "./index.css";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
     <div className="bg-white">
       <Switch>
         <Route exact path="/" component={Landingpage} />
+        <Route exact path="/login" component={SignIn} />
         {/* <Route exact path="/" component={Landingpage} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/forgot-password" component={Forgotpassword} />
